2e34e7399e46cc568e76b80fa5d752e705d0e37d
Bernardo-Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2023-03-10 12:19:29 -0300

    build: add seed to server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import config from "./config.js";
 import app from "./app.js";
+import seed from "../prisma/seed-production.js";
 
 app.listen(config.port, () => {
+  seed();
   console.log(`App listening on PORT ${config.port}`);
 });
